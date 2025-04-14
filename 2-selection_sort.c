3ff0ca43083a4085adf824c4306846026f871ef9
HectorPR4546 <h_perez93@yahoo.com>
2025-04-14 14:01:02 -0400

    Task 2

---
+++ 2-selection_sort.c
@@ -0,0 +1,39 @@
+#include "sort.h"
+
+/**
+ * selection_sort - Sorts an array of integers in ascending order
+ * @array: Array to sort
+ * @size: Size of the array
+ */
+void selection_sort(int *array, size_t size)
+{
+	size_t i;
+	size_t j;
+	size_t h;
+	int tmp;
+	int swap = 0;
+
+	if (array)
+	{
+		for (i = 0; i < size ; i++)
+		{
+			tmp = array[i];
+			for (j = i; j < size; j++)
+			{
+				if (tmp > array[j])
+				{
+					tmp = array[j];
+					h = j;
+					swap = 1;
+				}
+				if (j + 1 == size && swap == 1)
+				{
+					array[h] = array[i];
+					array[i] = tmp;
+					print_array(array, size);
+					swap = 0;
+				}
+			}
+		}
+	}
+}
